53240e9645f238187613070788a81d8d18a79570
Samuel Popelar <xpopel22@vutbr.cz>
2023-04-24 13:23:07 +0200

    flood fill anim done

--- flood_fill.py
+++ flood_fill.py
@@ -6,18 +6,24 @@ def flood_fill(arr_numbers, x, y):
         return arr_numbers
     if arr_numbers[x, y] == 0 or arr_numbers[x, y] == 2:
         return arr_numbers
+
+    plt.imshow(arr_numbers, cmap="gray")
+    plt.show(block=False)
+    plt.pause(0.02)
+    plt.clf()
+
     arr_numbers[x, y] = 2
-    flood_fill(arr_numbers, x + 1, y)
-    flood_fill(arr_numbers, x - 1, y)
-    flood_fill(arr_numbers, x, y + 1)
-    flood_fill(arr_numbers, x, y - 1)
+    arr_numbers = flood_fill(arr_numbers, x + 1, y)
+    arr_numbers = flood_fill(arr_numbers, x - 1, y)
+    arr_numbers = flood_fill(arr_numbers, x, y + 1)
+    arr_numbers = flood_fill(arr_numbers, x, y - 1)
     return arr_numbers
 
 
 def main():
     # img = plt.imread("files/img0.png")[:, :, 0]
-    # img = plt.imread("files/img1.png")[:, :, 0]
-    img = plt.imread("files/img2.png")[:, :, 0]
+    img = plt.imread("files/img1.png")[:, :, 0]
+    #img = plt.imread("files/img2.png")[:, :, 0]
 
     img = flood_fill(img, 0, 0)
 
