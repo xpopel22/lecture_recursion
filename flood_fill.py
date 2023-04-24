56a6fa8336dd990dc5dcf2f22ee0facb1578b73a
Samuel Popelar <xpopel22@vutbr.cz>
2023-04-24 13:17:52 +0200

    flood fill done

--- flood_fill.py
+++ flood_fill.py
@@ -1,12 +1,25 @@
 import matplotlib.pyplot as plt
 
 
-def main():
-    img = plt.imread("files/img0.png")[:, :, 0]
-    # img = plt.imread("files/img1.png")[:, :, 0]
-    # img = plt.imread("files/img2.png")[:, :, 0]
+def flood_fill(arr_numbers, x, y):
+    if x < 0 or y < 0 or x == arr_numbers.shape[0] or y == arr_numbers.shape[1]:
+        return arr_numbers
+    if arr_numbers[x, y] == 0 or arr_numbers[x, y] == 2:
+        return arr_numbers
+    arr_numbers[x, y] = 2
+    flood_fill(arr_numbers, x + 1, y)
+    flood_fill(arr_numbers, x - 1, y)
+    flood_fill(arr_numbers, x, y + 1)
+    flood_fill(arr_numbers, x, y - 1)
+    return arr_numbers
 
-    # img = floodfill(img, 0, 0)
+
+def main():
+    # img = plt.imread("files/img0.png")[:, :, 0]
+    # img = plt.imread("files/img1.png")[:, :, 0]
+    img = plt.imread("files/img2.png")[:, :, 0]
+
+    img = flood_fill(img, 0, 0)
 
     plt.imshow(img, cmap="gray")
     plt.show(block=False)
